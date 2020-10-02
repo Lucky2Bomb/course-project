a457e114936c506c7903759262506db62913295b
Lucky2Bomb <Lucky2Bomb@gmail.com>
2020-10-02 22:34:09 +0500

    add code is successful connection to MySQL DB by Sequelize.js

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
+// const {Sequelize, Model, DataTypes} = require("sequelize");
+// const sequelize = new Sequelize("course-shop_db", "root", "1234_Airat_qwerTy", {
+//     dialect: "mysql",
+//     host: "localhost"
+// });
+
+// async function testConnectionToMySQLDB() {
+//     try {
+//         await sequelize.authenticate();
+//         console.log('CONNECTION CONFIRMED!');
+//     } catch (error) {
+//         console.error('CONNECTION ERROR:', error);
+//     }
+// }
+// testConnectionToMySQLDB();
+
 const PORT = 3001;
+
 
 app.use(express.json({ extended: true }));
 app.use("/", require("./routes/main.routes"));
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,13 +1,53 @@
 const {Router} = require("express");
 const cors = require("cors");
 
+const {Sequelize, Model, DataTypes} = require("sequelize");
+const sequelize = new Sequelize("course-shop_db", "root", "1234_Airat_qwerTy", {
+    dialect: "mysql",
+    host: "localhost"
+});
+
+async function testConnectionToMySQLDB() {
+    try {
+        await sequelize.authenticate();
+        console.log('CONNECTION CONFIRMED!');
+    } catch (error) {
+        console.error('CONNECTION ERROR:', error);
+    }
+}
+testConnectionToMySQLDB();
+
+const Test = sequelize.define("Test", {
+    idtests: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        primaryKey: true
+    },
+    message: {
+        type: DataTypes.STRING(100)
+    }
+}, {
+    //disabled created colums 'createdAt' and 'updatedAt'
+    timestamps: false,
+    tableName: "mytesttable"
+});
+Test.sync(); 
+const testselectquest = async () => {
+    const testtable = await Test.findAll();
+    // console.log('TEST');
+    // console.log((JSON.stringify(testtable[0].message)));
+    return JSON.stringify(testtable[0].message);
+}
+
 const router = Router();
 
 router.get("/", cors(), async (req, res) => {
     try {
         // res.send("Hello, react.js! By Express.js");
         res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-        res.status(201).json({message: "Hello React.js! By Express.js"});
+        const testMessageBD = await testselectquest();
+        
+        res.status(201).json({message: "Hello React.js! By Express.js!" + ` ${testMessageBD.slice(1, testMessageBD.length - 2)}!`});
     } catch (error) {
         console.log("Oops! server error...");
         res.status(500).json({message: "Oops! server error..."});
